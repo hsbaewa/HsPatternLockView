fb89117d63051ab4c7b27809afb117509fef491c
hsbaewa <hsbaewa@gmail.com>
2017-01-12 14:21:21 +0900

    first commit

---
+++ app/src/main/java/kr/co/hs/view/hspatternlockview/sample/MainActivity.java
@@ -0,0 +1,20 @@
+package kr.co.hs.view.hspatternlockview.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 생성된 시간 2017-01-12, Bae 에 의해 생성됨
+ * 프로젝트 이름 : HsPatternLockView
+ * 패키지명 : kr.co.hs.view.hspatternlockview.sample
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}
